c1995cf52421eb3f4dee0d4c6db5f72a784fa539
Mihaly Ale <105678319+mihaly-ale@users.noreply.github.com>
2026-03-14 23:21:34 +0100

    fix: name field accepts space characters
    Before name field was able to take only one string as an input,as the
    regular expression did not allow whitepace.

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -31,7 +31,7 @@ const validateRegister = [
 		.trim()
 		.isLength({ min: 3, max: 30 })
 		.withMessage('El nombre de usuario debe tener entre 3 y 30 caracteres.')
-		.matches(/^[a-zA-Z0-9_.-]+$/)
+		.matches(/^[\sa-zA-Z0-9_.-]+$/)
 		.withMessage('Solo se permiten letras, números, guiones y puntos.'),
 
 	body('email')
